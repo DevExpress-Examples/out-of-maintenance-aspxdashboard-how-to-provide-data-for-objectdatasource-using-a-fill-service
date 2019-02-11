8398b69527a8bef9fc33df8e96116ffea5c61ea6
DevExpressExampleBot <fakeEmail>
2019-02-11 18:08:18 +0300

    Source auto update [skip ci]

---
+++ VB/DXWebApplication5/CustomObjectDataSourceCustomFillService.vb
@@ -0,0 +1,33 @@
+﻿Imports DevExpress.DashboardCommon
+Imports System.Collections.Generic
+Imports System.Data
+
+Namespace DXWebApplication5
+    Public Class CustomObjectDataSourceCustomFillService
+        Implements IObjectDataSourceCustomFillService
+
+        Public Function GetData(ByVal dataSource As DashboardObjectDataSource, ByVal fillParameters As ObjectDataSourceFillParameters) As Object
+            If fillParameters.DataFields Is Nothing OrElse fillParameters.DataFields.Length = 0 Then
+                Return Nothing
+            End If
+
+            Dim data As List(Of SalesPersonData) = DataGenerator.CreateSourceData()
+
+            Dim table As New DataTable()
+            For Each field As String In fillParameters.DataFields
+                table.Columns.Add(field)
+            Next field
+            For i As Integer = 0 To data.Count - 1
+                Dim row(fillParameters.DataFields.Length - 1) As Object
+                For j As Integer = 0 To fillParameters.DataFields.Length - 1
+                    row(j) = GetPropertyValue(data(i), fillParameters.DataFields(j))
+                Next j
+                table.Rows.Add(row)
+            Next i
+            Return table.DefaultView
+        End Function
+        Private Function GetPropertyValue(ByVal obj As SalesPersonData, ByVal propName As String) As Object
+            Return If(propName = "SalesPerson", DirectCast(obj.SalesPerson, Object), DirectCast(obj.Quantity, Object))
+        End Function
+    End Class
+End Namespace
---
+++ VB/DXWebApplication5/SalesPersonData.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+
+Namespace DXWebApplication5
+    Public Class SalesPersonData
+        Public Property SalesPerson() As String
+        Public Property Quantity() As Integer
+    End Class
+
+    Public NotInheritable Class DataGenerator
+
+        Private Sub New()
+        End Sub
+
+        Public Shared Function CreateSourceData() As List(Of SalesPersonData)
+            Dim data As New List(Of SalesPersonData)()
+            Dim salesPersons() As String = { "Andrew Fuller", "Michael Suyama", "Robert King", "Nancy Davolio", "Margaret Peacock", "Laura Callahan", "Steven Buchanan", "Janet Leverling" }
+
+            For i As Integer = 0 To 99
+                Dim record As New SalesPersonData()
+                Dim seed As Integer = CInt(Date.Now.Ticks) And &HFFFF
+                record.SalesPerson = salesPersons((New Random(seed)).Next(0, salesPersons.Length))
+                record.Quantity = (New Random(seed)).Next(0, 100)
+                data.Add(record)
+            Next i
+            Return data
+        End Function
+    End Class
+End Namespace
